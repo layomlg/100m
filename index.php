6dc7b15dabfa27ba8d2208b33dd3cf08946903bf
Layator <omarpelayo.cubo1221@gmail.com>
2019-08-02 03:47:16 -0500

    sonidos

--- index.php
+++ index.php
@@ -13,6 +13,18 @@
   </head>
 
   <body>
+    <div class="sonidos">
+      <audio id="inicio">
+        <source src="sounds/inicio.mp3" type="audio/mpeg">
+      </audio>
+      <audio id="respuesta">
+        <source src="sounds/respuesta.mp3" type="audio/mpeg">
+      </audio>
+      <audio id="error">
+        <source src="sounds/error.mp3" type="audio/mpeg">
+      </audio>
+
+    </div>
     <div class="fullscreen">
       <button id="full1" onclick="launchFullScreen(document.documentElement);"><i class="fas fa-expand-arrows-alt"></i> </button>
       <button id="full2" onclick="cancelFullScreen();"> <i class="fas fa-expand-arrows-alt"></i> </button>
@@ -32,13 +44,23 @@
 
       <div class="preguntas">
         <p class="pregunta">¿Cómo puedes consultar los puntos con los que cuentas?</p>
-        <p class="respuesta">1) <span>En el sitio del programa</span><button>abrir</button></p>
-        <p class="respuesta">2) <span>*************************</span><button>abrir</button></p>
-        <p class="respuesta">3) <span>Enviando un correo a la cuenta del programa</span><button>abrir</button></p>  
+        <p class="respuesta">1) <span>En el sitio del programa</span><button onclick="acumular(30);" class="open o30"><i class="fa fa-check" aria-hidden="true"></i></button></p>
+        <p class="respuesta">2) <span>*************************</span><button onclick="acumular(20);" class="open"><i class="fa fa-check" aria-hidden="true"></i></button></p>
+        <p class="respuesta">3) <span>Enviando un correo a la cuenta del programa</span><button onclick="acumular(10);" class="open"><i class="fa fa-check" aria-hidden="true"></i></button></p>
+        <div class="error">
+          <i class="fa fa-times" aria-hidden="true"></i>
+          <i class="fa fa-times" aria-hidden="true"></i>
+          <i class="fa fa-times" aria-hidden="true"></i>          
+        </div>  
       </div>
-      <div class="pts t-acum"><span>165</span></div>
-      <div class="pts t-azul"><span>95</span></div>
+      <div class="botones">
+        <button onclick="acumrojo();" class="b-rojo"><i class="fa fa-arrow-left" aria-hidden="true"></i></button>
+        <button onclick="error();" class="b-mal"><i class="fa fa-times" aria-hidden="true"></i></button>
+        <button onclick="acumazul();" class="b-azul"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+      </div>
+      <div class="pts t-acum"><span>0</span></div>
       <div class="pts t-rojo"><span>0</span></div>
+      <div class="pts t-azul"><span>0</span></div>
     </div>
 
 
@@ -47,7 +69,11 @@
 
 
     <!-- JS Scripts -->
+    <script src="js/jquery-3.4.1.min.js"></script>
     <script>
+      var acumulado = 0;
+      var ptsRojo = 0;
+      var ptsAzul = 0;
       function launchFullScreen(element) {
         if(element.requestFullScreen) {
           element.requestFullScreen();
@@ -71,8 +97,59 @@
         document.getElementById("full2").style.display = "none";
         document.getElementById("full1").style.display = "block";
       }
-    </script>
 
+
+      function acumular(acum) {
+        acumulado = acumulado + acum;
+        $(".pts.t-acum").html(acumulado);
+        document.getElementById("respuesta").play();
+      }
+      function acumrojo() {
+        ptsRojo = ptsRojo + acumulado;
+        acumulado = 0;
+        $(".pts.t-acum").html(acumulado);
+        $(".pts.t-rojo").html(ptsRojo);
+        $(".open").show();
+        $(".error").removeClass("uno");
+        $(".error").removeClass("dos");
+        $(".error").removeClass("tres");
+        document.getElementById("inicio").play();
+      }
+      function acumazul() {
+        ptsAzul = ptsAzul + acumulado;
+        acumulado = 0;
+        $(".pts.t-acum").html(acumulado);
+        $(".pts.t-azul").html(ptsAzul);
+        $(".open").show();
+        $(".error").removeClass("uno");
+        $(".error").removeClass("dos");
+        $(".error").removeClass("tres");
+        document.getElementById("inicio").play();
+      }
+
+      function error() {
+
+        if($(".error").hasClass("uno")){
+          if($(".error").hasClass("dos")){
+            $(".error").addClass("tres");
+          }
+          else{
+            $(".error").addClass("dos");
+          }
+
+        }
+        else{
+          $(".error").addClass("uno");
+        }
+        document.getElementById("error").play();
+      }
+
+
+      $(".open").click(function(){
+        $(this).toggle();
+      });
+    </script>
   </body>
+
 
 </html>
